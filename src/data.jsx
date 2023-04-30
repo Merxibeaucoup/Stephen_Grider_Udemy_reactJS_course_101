d6fdbe6fc47edda76ca4a61a312c686ad8d48948
Edgar Briandt <ebriandt_6243@email.ric.edu>
2023-04-29 21:35:55 -0400

    images added

--- src/data.jsx
+++ src/data.jsx
@@ -1,21 +1,21 @@
 const cardData = [
   {
     id: 1,
-    img: "./assets/images/monkey.jpg",
+    img: "./assets/images/alexa.png",
     title: "Alexa",
     twitter: "@alexa99",
     description: "I will help you buy stuff off Amazon",
   },
   {
     id: 2,
-    img: "./assets/images/monkey.jpg",
+    img: "./assets/images/cortana.png",
     title: "Cortana",
     twitter: "@cortana12",
     description: "Personal assisstant by Microsoft",
   },
   {
     id: 3,
-    img: "./assets/images/monkey.jpg",
+    img: "./assets/images/siri.png",
     title: "Siri",
     twitter: "@siri44",
     description: "I dont get alot of updates anymore",
